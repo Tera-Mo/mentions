bc212ddd14b25e89b7456d0896cf803d6aab69fa
Tera-Mo <mohamedantar4810@gmail.com>
2025-04-03 23:35:04 +0400

    final update

--- index.js
+++ index.js
@@ -1,31 +1,61 @@
-const decreasebtn = document.getElementById("decreasebtn");
-const resetbtn = document.getElementById("resetbtn");
-const increasebtn = document.getElementById("increasebtn");
-const countlabel = document.getElementById("countlabel");
-let count = 0;
+const btn1 = document.getElementById("btn1");
+const btn2 = document.getElementById("btn2");
+const btn3 = document.getElementById("btn3");
+const btn4 = document.getElementById("btn4");
+const btn5 = document.getElementById("btn5");
+let count1 = 0;
+let count2 = 0;
+let count3 = 0;
+let count4 = 0;
+let count5 = 0;
+//1
+document.getElementById("btn1").onclick = function(){
+    count1++;
+    document.getElementById("mention-1").textContent = count1;
+}
 
-increasebtn.onclick = function(){
-    count++;
-    countlabel.textContent = count;
+document.getElementById("btn11").onclick = function(){
+    count1--;
+    document.getElementById("mention-1").textContent = count1;
 }
-decreasebtn.onclick = function(){
-    count--;
-    countlabel.textContent = count;
+
+//2
+document.getElementById("btn2").onclick = function(){
+    count2++;
+    document.getElementById("mention-2").textContent = count2;
 }
-resetbtn.onclick = function(){
-    count = 0;
-    countlabel.textContent = count;
+
+document.getElementById("btn21").onclick = function(){
+    count2--;
+    document.getElementById("mention-2").textContent = count2;
 }
-//mentions
-document.getElementById("mention1").onclick = function(){
-    document.getElementById("h1").textContent = "اللهم صل و سلم على سيدنا محمد";
+//3
+document.getElementById("btn3").onclick = function(){
+    count3++;
+    document.getElementById("mention-3").textContent = count3;
 }
-document.getElementById("mention2").onclick = function(){
-    document.getElementById("h1").textContent = "سبحان الله";
+
+document.getElementById("btn31").onclick = function(){
+    count3--;
+    document.getElementById("mention-3").textContent = count3;
 }
-document.getElementById("mention3").onclick = function(){
-    document.getElementById("h1").textContent = "الحمدلله";
+//4
+document.getElementById("btn4").onclick = function(){
+    count4++;
+    document.getElementById("mention-4").textContent = count4;
 }
-document.getElementById("mention4").onclick = function(){
-    document.getElementById("h1").textContent = "الله أكبر";
+
+document.getElementById("btn41").onclick = function(){
+    count4--;
+    document.getElementById("mention-4").textContent = count4;
+}
+//5
+document.getElementById("btn5").onclick = function(){
+    count5++;
+    document.getElementById("mention-5").textContent = count5;
+}
+
+document.getElementById("btn51").onclick = function(){
+    count5--;
+    document.getElementById("mention-5").textContent = count5;
 }
