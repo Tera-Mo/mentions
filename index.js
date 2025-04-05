5ac340a1c43d75d860e2a0ffcdc7e3a4871f1977
Tera-Mo <mohamedantar4810@gmail.com>
2025-04-05 19:29:25 +0400

    big update

--- index.js
+++ index.js
@@ -19,6 +19,11 @@ document.getElementById("btn11").onclick = function(){
     document.getElementById("mention-1").textContent = count1;
 }
 
+document.getElementById("zero1").onclick = function(){
+    count1 = 0;
+    document.getElementById("mention-1").textContent = count1;
+}
+
 //2
 document.getElementById("btn2").onclick = function(){
     count2++;
@@ -28,6 +33,11 @@ document.getElementById("btn2").onclick = function(){
 document.getElementById("btn21").onclick = function(){
     count2--;
     document.getElementById("mention-2").textContent = count2;
+}
+
+document.getElementById("zero2").onclick = function(){
+    count1 = 0;
+    document.getElementById("mention-2").textContent = count1;
 }
 //3
 document.getElementById("btn3").onclick = function(){
@@ -39,6 +49,11 @@ document.getElementById("btn31").onclick = function(){
     count3--;
     document.getElementById("mention-3").textContent = count3;
 }
+
+document.getElementById("zero3").onclick = function(){
+    count1 = 0;
+    document.getElementById("mention-3").textContent = count1;
+}
 //4
 document.getElementById("btn4").onclick = function(){
     count4++;
@@ -49,6 +64,11 @@ document.getElementById("btn41").onclick = function(){
     count4--;
     document.getElementById("mention-4").textContent = count4;
 }
+
+document.getElementById("zero4").onclick = function(){
+    count1 = 0;
+    document.getElementById("mention-4").textContent = count1;
+}
 //5
 document.getElementById("btn5").onclick = function(){
     count5++;
@@ -58,4 +78,9 @@ document.getElementById("btn5").onclick = function(){
 document.getElementById("btn51").onclick = function(){
     count5--;
     document.getElementById("mention-5").textContent = count5;
+}
+
+document.getElementById("zero5").onclick = function(){
+    count1 = 0;
+    document.getElementById("mention-5").textContent = count1;
 }
